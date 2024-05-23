2393d6ac8f04b3ef92a22b92a78aa5471650c68e
Kobita Nergala <bigjapko@gmail.com>
2024-05-23 16:36:25 +0200

    initial project tweaks sets the lib and ui projects up

--- PostCloneUI/Dashboard.Designer.cs
+++ PostCloneUI/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PostCloneUI
 {
-    partial class Form1
+    partial class Dashboard
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Dashboard";
+            Text = "PostmanClone";
+            ResumeLayout(false);
         }
 
         #endregion
